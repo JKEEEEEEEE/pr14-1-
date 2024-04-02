f9e58a2ee4b6ad6f7116b0b7f4bfc71e1b7a8e59
denis <1@1.1>
2024-04-03 00:59:47 +0300

    исправление ошибок

--- Models/kursach_diplomContext.cs
+++ Models/kursach_diplomContext.cs
@@ -25,14 +25,7 @@ namespace diplom_api.Models
         public virtual DbSet<TouristRoute> TouristRoutes { get; set; } = null!;
         public virtual DbSet<User> Users { get; set; } = null!;
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Data Source=SHADOURAZE\\SQLEXPRESS;Initial Catalog=kursach_diplom;Integrated Security=True");
-            }
-        }
+
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
